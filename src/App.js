83b15cacd77c34d54a3e8d3606c18b3c6114dbbc
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2021-06-20 01:30:38 +0200

    Created routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import { AuthContext, useAuthInit } from "./firebase/auth";
+import { FormDataProvider } from "./context/FormData";
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+import DataAnal from "./pages/DataAnal"
+import Security from "./pages/Security"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
+  const authState = useAuthInit();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={{ loggedIn: authState.loggedIn }}>
+      <FormDataProvider>
+        <HashRouter>
+            <>
+              <Route exact path="/" component={Home}></Route>
+              <Route path="/login" component={Login}></Route>
+              <Route path="/register" component={Register}></Route>
+              <Route path="/about" component={About}></Route>
+              <Route path="/security" component={Security}></Route>
+              <Route path="/data" component={DataAnal}></Route>
+            </>
+          </HashRouter>
+      </FormDataProvider>
+    </AuthContext.Provider>
   );
 }
 
