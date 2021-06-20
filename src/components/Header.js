cfd366627a64f54e82bb06b7e2c508b179327722
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2021-06-20 22:21:52 +0200

    Added scroll effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-scroll";
 
 const Header = () => {
   return (
@@ -8,7 +11,16 @@ const Header = () => {
           The internet is not only for the Big Players. <br /> Why should
           cybersecurity be any different?
         </h1>
+
         <h2 className="sub-header">cybersecurity for small businesses</h2>
+        <button className="header-button">
+          <Link to="welcome" smooth={true} duration={500}>
+            <FontAwesomeIcon
+              className="header-icon"
+              icon={faChevronCircleDown}
+            />
+          </Link>
+        </button>
       </div>
     </div>
   );
