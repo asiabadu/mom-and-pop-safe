f708cfb768ac62e425fc7f6f7af7083c39d22d1f
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2021-06-20 21:00:25 +0200

    Created header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="banner">
+      <div className="header-texts">
+        <h1 className="main-header">
+          The internet is not only for the Big Players. <br /> Why should
+          cybersecurity be any different?
+        </h1>
+        <h2 className="sub-header">cybersecurity for small businesses</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
