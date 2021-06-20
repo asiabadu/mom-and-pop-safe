dda37f12228bf09d309689406724c62e25bde922
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2021-06-20 22:22:11 +0200

    Created section

---
+++ src/components/Welcome.js
@@ -0,0 +1,22 @@
+import React from "react";
+function Welcome() {
+  return (
+    <div id="welcome" className="welcome-section">
+      <div className="welcome-section-content">
+        <h2>welcome</h2>
+        <span className="horizontal-line">
+          <hr></hr>
+        </span>
+        <p>
+          Cupcake ipsum dolor sit amet chocolate cake chocolate. Croissant
+          tootsie roll pudding fruitcake jelly-o oat cake carrot cake. Toffee
+          caramels icing. Icing pudding I love sugar plum apple pie dragée candy
+          lollipop. Fruitcake chocolate cake pastry I love marzipan lemon drops
+          sweet roll. Sweet roll lemon drops gummi bears. Oat cake jelly beans
+          sugar plum.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Welcome;
