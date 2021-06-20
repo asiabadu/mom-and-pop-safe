570d6a436b6d91b85d7c560461efa23b6c585731
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2021-06-21 01:36:47 +0200

    Added components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,10 @@ import React from "react";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Welcome from "../components/Welcome";
+import Columns from "../components/Columns";
+import Offer from "../components/Offer";
+import Video from "../components/Video";
+import Clients from "../components/Clients";
 
 function Home() {
   return (
@@ -12,6 +16,10 @@ function Home() {
       </header>
       <main>
         <Welcome></Welcome>
+        <Columns></Columns>
+        <Offer></Offer>
+        <Video></Video>
+        <Clients></Clients>
       </main>
       <footer></footer>
     </>
