0af9110666b4e90aa0e5eff43ddc1e63c797bf68
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2021-06-21 14:41:15 +0200

    Created section

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <h2 className="footer-text">let's work together</h2>
+        <span className="horizontal-line">
+          <hr></hr>
+        </span>
+        <button className="footer-btn">CONTACT US</button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
