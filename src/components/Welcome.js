12ed88732ad76a4e046dc798ee9533afca4c4c9d
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2021-06-21 22:09:45 +0200

    Added props

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,20 +1,13 @@
 import React from "react";
-function Welcome() {
+function Welcome(props) {
   return (
     <div id="welcome" className="welcome-section">
       <div className="welcome-section-content">
-        <h2>welcome</h2>
+        <h2>{props.header}</h2>
         <span className="horizontal-line">
           <hr></hr>
         </span>
-        <p>
-          Cupcake ipsum dolor sit amet chocolate cake chocolate. Croissant
-          tootsie roll pudding fruitcake jelly-o oat cake carrot cake. Toffee
-          caramels icing. Icing pudding I love sugar plum apple pie dragée candy
-          lollipop. Fruitcake chocolate cake pastry I love marzipan lemon drops
-          sweet roll. Sweet roll lemon drops gummi bears. Oat cake jelly beans
-          sugar plum.
-        </p>
+        <p>{props.text}</p>
       </div>
     </div>
   );
